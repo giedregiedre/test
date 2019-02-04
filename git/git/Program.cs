2a0442d0f61efcf817b1c7249a1fbe503f284c1f
etgcvskmnhrcgxyd3mfo <giedrejakucionyte8@gmail.com>
2019-02-04 20:45:30 +0200

    5)

--- git/git/Program.cs
+++ git/git/Program.cs
@@ -10,7 +10,93 @@ namespace git
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Irasykite tris skaicius: ");
+            int pirmas = Convert.ToInt32(Console.ReadLine());
+            int antras = Convert.ToInt32(Console.ReadLine());
+            int trecias = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Max(pirmas, antras, trecias));
+            Console.WriteLine(InRange(pirmas, antras, trecias));
+            Console.WriteLine(IsPrimal(pirmas));
+            Console.WriteLine(D20(pirmas));
+            Console.WriteLine(D10(pirmas));
+            Console.WriteLine(D6(pirmas));
+            Console.WriteLine(NuoIki(pirmas, antras));
+
+            Console.ReadKey();
+        }
+        static string Max(int pirmas, int antras, int trecias)
+        {
+
+            if (pirmas > antras && pirmas > trecias)
+            {
+                return "Didziausias pirmas ivestas sakicius";
+            }
+            else if (antras > pirmas && antras > trecias)
+            {
+                return "Didziausias antras ivestas sakicius";
+            }
+            else if (trecias > pirmas && trecias > antras)
+            {
+                return "Didziausias trecais ivestas sakicius";
+            }
+            else
+                return "Kazkurie skaiciai vienodi";
+        }
+        static bool InRange(int pirmas, int antras, int trecias)
+        {
+            if (pirmas < antras && pirmas > trecias)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static bool IsPrimal(int pirmas)
+        {
+            for (int i = 0; i < pirmas; i++)
+            {              
+                if (pirmas < 1 || (pirmas % i) == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        static int D20(int skaicius)
+        {
+            skaicius = new Random().Next(0, 20);
+            return skaicius;
+        }
+        static int D10(int skaicius)
+        {
+            skaicius = new Random().Next(0, 10);
+            return skaicius;
+        }
+        static int D6(int skaicius)
+        {
+            skaicius = new Random().Next(0, 6);
+            return skaicius;
+        }
+        static int NuoIki(int pirmas, int antras)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Iveskite skaiciu nuo " + pirmas + " iki " + antras);
+                int skaicius = Convert.ToInt32(Console.ReadLine());
+
+                if (skaicius < pirmas || skaicius > antras)
+                {
+                    Console.WriteLine("Ivedimas neteisingas. Veskite is naujo");
+                }
+                else
+                {
+                    return skaicius;
+                }
+            }
         }
     }
 }
