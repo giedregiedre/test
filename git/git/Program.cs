edfa249c7c6173e73378dd6eef0ede7810e17d68
etgcvskmnhrcgxyd3mfo <giedrejakucionyte8@gmail.com>
2019-02-04 20:30:24 +0200

    daugiau pakeitimu

--- git/git/Program.cs
+++ git/git/Program.cs
@@ -16,6 +16,11 @@ namespace git
             int trecias = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine(Max(pirmas, antras, trecias));
+            Console.WriteLine(InRange(pirmas, antras, trecias));
+            Console.WriteLine(IsPrimal(pirmas));
+            Console.WriteLine(D20(pirmas));
+            Console.WriteLine(D10(pirmas));
+            Console.WriteLine(D6(pirmas));
 
             Console.ReadKey();
         }
@@ -36,6 +41,43 @@ namespace git
             }
             else
                 return "Kazkurie skaiciai vienodi";
+        }
+        static bool InRange(int pirmas, int antras, int trecias)
+        {
+            if (pirmas < antras && pirmas > trecias)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static bool IsPrimal(int pirmas)
+        {
+            for (int i = 0; i < pirmas; i++)
+            {              
+                if (pirmas < 1 || (pirmas % i) == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        static int D20(int skaicius)
+        {
+            skaicius = new Random().Next(0, 20);
+            return skaicius;
+        }
+        static int D10(int skaicius)
+        {
+            skaicius = new Random().Next(0, 10);
+            return skaicius;
+        }
+        static int D6(int skaicius)
+        {
+            skaicius = new Random().Next(0, 6);
+            return skaicius;
         }
     }
 }
