e44844f86e7f8f165d0c31e4e696c989bc6759e9
etgcvskmnhrcgxyd3mfo <giedrejakucionyte8@gmail.com>
2019-02-04 19:52:08 +0200

    Max funkcija

--- git/git/Program.cs
+++ git/git/Program.cs
@@ -10,8 +10,32 @@ namespace git
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Irasykite tris skaicius: ");
+            int pirmas = Convert.ToInt32(Console.ReadLine());
+            int antras = Convert.ToInt32(Console.ReadLine());
+            int trecias = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Max(pirmas, antras, trecias));
+
+            Console.ReadKey();
+        }
+        static string Max(int pirmas, int antras, int trecias)
+        {
+
+            if (pirmas > antras && pirmas > trecias)
+            {
+                return "Didziausias pirmas ivestas sakicius";
+            }
+            else if (antras > pirmas && antras > trecias)
+            {
+                return "Didziausias antras ivestas sakicius";
+            }
+            else if (trecias > pirmas && trecias > antras)
+            {
+                return "Didziausias trecais ivestas sakicius";
+            }
+            else
+                return "Kazkurie skaiciai vienodi";
         }
     }
 }
